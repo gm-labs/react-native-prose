066a06db19d604b4ebc00b202822374e3f058d94
wyilio <williamwang749@gmail.com>
2025-06-16 17:16:03 -0400

    feat: integrate example app with prose

--- src/Text.tsx
+++ src/Text.tsx
@@ -10,7 +10,7 @@ import {
 } from 'react-native'
 
 const LINKING_ERROR =
-  `The package 'react-native-uitextview' doesn't seem to be linked. Make sure: \n\n` +
+  `The package 'react-native-prose' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ios: "- You have run 'pod install'\n", default: ''}) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n'
